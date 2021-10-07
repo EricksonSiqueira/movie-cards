85f9315b714880abe7e9edb5370d6fa7ca3dc86f
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-06 22:06:00 -0300

    Adicionando filmes ao movies

--- src/data.js
+++ src/data.js
@@ -1,40 +1,5 @@
 const movies = [
   {
-    title: 'Kingsglaive',
-    subtitle: 'Final Fantasy XV',
-    storyline: 'King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire\'s plans to steal the sacred crystal.',
-    rating: 4.5,
-    imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
-  },
-  {
-    title: 'Final Fantasy',
-    subtitle: 'Spirits Within',
-    storyline: 'A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.',
-    rating: 4.5,
-    imagePath: 'images/Final_Fantasy_Spirits_Within.jpg',
-  },
-  {
-    title: 'Ghost In The Shell',
-    subtitle: 'Ghost In The Shell',
-    storyline: 'A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).',
-    rating: 5,
-    imagePath: 'images/Ghost_In_The_Shell_2_0.jpg',
-  },
-  {
-    title: 'Appleseed Alpha',
-    subtitle: 'Appleseed Alpha',
-    storyline: 'A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human\'s future hope, the legendary city of Olympus.',
-    rating: 3.8,
-    imagePath: 'images/Appleseed_Alpha.jpg',
-  },
-  {
-    title: 'Resident Evil',
-    subtitle: 'Vendetta',
-    storyline: 'Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.',
-    rating: 4.2,
-    imagePath: 'images/Resident_Evil_Vendetta.jpg',
-  },
-  {
     title: 'Warcraft: O Primeiro Encontro de Dois Mundos',
     subtitle: 'Warcraft',
     storyline: 'O mundo dos orcs está à beira da destruição.Liderados pelo mago Guldan, eles entram num portal que os leva a Azeroth, onde se envolvem num conflito com os humanos locais pelo território de Ventobravo',
@@ -45,8 +10,36 @@ const movies = [
     title: 'Avengers: Endgame',
     subtitle: 'Endgame',
     storyline: 'Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.',
-    rating: 4.6,
+    rating: 4.8,
     imagePath: 'https://c7nema.net/wp-content/uploads/2019/04/naom_5cd55a3f25614.jpg',
+  },
+  {
+    title: 'Interestelar',
+    subtitle: 'Interestelar',
+    storyline: 'As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar.',
+    rating: 4.9,
+    imagePath: 'https://lh3.googleusercontent.com/proxy/1IeSU7OttCBOzdrt3d9oMwG3Ldp0xMcS2uwg9kj8Gdnq5IZsMlMfHnbSo9J12yqLHukieiFb81-yeZhUsfUAVY-g_FvV27Vs2-YRdArFt00Q_XuiBbqHMA',
+  },
+  {
+    title: 'El Camino',
+    subtitle: 'El Camino',
+    storyline: 'Após fugir do cativeiro, onde foi mantido quando sequestrado, dramaticamente, Jesse Pinkman (Aaron Paul) inicia uma jornada em busca da própria liberdade, mas antes precisa se reconciliar com o passado para, só então, ter seu futuro garantido.',
+    rating: 4.3,
+    imagePath: 'https://cdn.fstatic.com/media/movies/covers/2019/09/jesse.jpg',
+  },
+  {
+    title: 'Homem-Aranha no Aranhaverso',
+    subtitle: 'Aranhaverso',
+    storyline: 'Após ser atingido por uma teia radioativa, Miles Morales, um jovem negro do Brooklyn, se torna o Homem-Aranha, inspirado no legado do já falecido Peter Parker. Entretanto, ao visitar o túmulo de seu ídolo em uma noite chuvosa, ele é surpreendido com a presença do próprio Peter, vestindo o traje do herói por baixo de um sobretudo. A surpresa fica ainda maior quando Miles descobre que ele veio de uma dimensão paralela, assim como outras versões do Homem-Aranha.',
+    rating: 4.2,
+    imagePath: 'https://a-static.mlcdn.com.br/1500x1500/painel-de-festa-homem-aranha-no-aranhaverso-05-colormyhome/colormyhome/1145/df06c9862f962ab3807c0ea391e42c84.jpg',
+  },
+  {
+    title: 'Princesa Mononoke',
+    subtitle: 'Mononoke Hime',
+    storyline: 'Um príncipe, em busca de uma cura, luta em uma guerra entre a mata e uma colônia mineira. Nesta aventura ele conhece Mononoke.',
+    rating: 5,
+    imagePath: 'https://images4.alphacoders.com/204/thumb-1920-204036.jpg',
   },
 ];
 
