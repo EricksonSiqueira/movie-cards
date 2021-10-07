47812fe519573d5e636b6546b93fae90c0f627f9
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-06 23:07:10 -0300

    Estilizando projeto

--- src/data.js
+++ src/data.js
@@ -7,18 +7,18 @@ const movies = [
     imagePath: 'https://cdn.tlc-massive.com/shain/v1/dataservice/ResizeImage/$value?Format=%27png%27&Quality=85&ImageId=%27251486.png%27&ImageUrl=%27251486.png%27&EntityType=%27Item%27&EntityId=%273725%27&Width=1920&Height=1080&device=web_browser&subscriptions=Anonymous',
   },
   {
+    title: 'Princesa Mononoke',
+    subtitle: 'Mononoke Hime',
+    storyline: 'Após enfrentar um deus javali enfurecido, o príncipe Ashitaka é amaldiçoado com um mal que pode matá-lo. Para encontrar a cura, ele decide viajar para longe e acaba se envolvendo numa batalha entre os deuses animais da floresta e os moradores de uma vila de mineiros, que aos poucos estão destruindo a floresta.',
+    rating: 5,
+    imagePath: 'https://images4.alphacoders.com/204/thumb-1920-204036.jpg',
+  },
+  {
     title: 'Avengers: Endgame',
     subtitle: 'Endgame',
     storyline: 'Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.',
     rating: 4.8,
     imagePath: 'https://c7nema.net/wp-content/uploads/2019/04/naom_5cd55a3f25614.jpg',
-  },
-  {
-    title: 'Interestelar',
-    subtitle: 'Interestelar',
-    storyline: 'As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar.',
-    rating: 4.9,
-    imagePath: 'https://lh3.googleusercontent.com/proxy/1IeSU7OttCBOzdrt3d9oMwG3Ldp0xMcS2uwg9kj8Gdnq5IZsMlMfHnbSo9J12yqLHukieiFb81-yeZhUsfUAVY-g_FvV27Vs2-YRdArFt00Q_XuiBbqHMA',
   },
   {
     title: 'El Camino',
@@ -28,18 +28,18 @@ const movies = [
     imagePath: 'https://cdn.fstatic.com/media/movies/covers/2019/09/jesse.jpg',
   },
   {
+    title: 'Interestelar',
+    subtitle: 'Interestelar',
+    storyline: 'As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar.',
+    rating: 4.9,
+    imagePath: 'https://lh3.googleusercontent.com/proxy/1IeSU7OttCBOzdrt3d9oMwG3Ldp0xMcS2uwg9kj8Gdnq5IZsMlMfHnbSo9J12yqLHukieiFb81-yeZhUsfUAVY-g_FvV27Vs2-YRdArFt00Q_XuiBbqHMA',
+  },
+  {
     title: 'Homem-Aranha no Aranhaverso',
     subtitle: 'Aranhaverso',
     storyline: 'Após ser atingido por uma teia radioativa, Miles Morales, um jovem negro do Brooklyn, se torna o Homem-Aranha, inspirado no legado do já falecido Peter Parker. Entretanto, ao visitar o túmulo de seu ídolo em uma noite chuvosa, ele é surpreendido com a presença do próprio Peter, vestindo o traje do herói por baixo de um sobretudo. A surpresa fica ainda maior quando Miles descobre que ele veio de uma dimensão paralela, assim como outras versões do Homem-Aranha.',
     rating: 4.2,
     imagePath: 'https://a-static.mlcdn.com.br/1500x1500/painel-de-festa-homem-aranha-no-aranhaverso-05-colormyhome/colormyhome/1145/df06c9862f962ab3807c0ea391e42c84.jpg',
-  },
-  {
-    title: 'Princesa Mononoke',
-    subtitle: 'Mononoke Hime',
-    storyline: 'Um príncipe, em busca de uma cura, luta em uma guerra entre a mata e uma colônia mineira. Nesta aventura ele conhece Mononoke.',
-    rating: 5,
-    imagePath: 'https://images4.alphacoders.com/204/thumb-1920-204036.jpg',
   },
 ];
 
