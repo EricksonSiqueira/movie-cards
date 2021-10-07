292893cc107f913acc5a801915aeb0db1c7ff34a
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-06 21:50:59 -0300

    Pegando arquivos do repositorio da trybe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MovieList from './components/MovieList';
+import moviesData from './data';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      <Header />
+      <MovieList movies={ moviesData } />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,53 @@
+const movies = [
+  {
+    title: 'Kingsglaive',
+    subtitle: 'Final Fantasy XV',
+    storyline: 'King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire\'s plans to steal the sacred crystal.',
+    rating: 4.5,
+    imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
+  },
+  {
+    title: 'Final Fantasy',
+    subtitle: 'Spirits Within',
+    storyline: 'A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.',
+    rating: 4.5,
+    imagePath: 'images/Final_Fantasy_Spirits_Within.jpg',
+  },
+  {
+    title: 'Ghost In The Shell',
+    subtitle: 'Ghost In The Shell',
+    storyline: 'A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).',
+    rating: 5,
+    imagePath: 'images/Ghost_In_The_Shell_2_0.jpg',
+  },
+  {
+    title: 'Appleseed Alpha',
+    subtitle: 'Appleseed Alpha',
+    storyline: 'A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human\'s future hope, the legendary city of Olympus.',
+    rating: 3.8,
+    imagePath: 'images/Appleseed_Alpha.jpg',
+  },
+  {
+    title: 'Resident Evil',
+    subtitle: 'Vendetta',
+    storyline: 'Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.',
+    rating: 4.2,
+    imagePath: 'images/Resident_Evil_Vendetta.jpg',
+  },
+  {
+    title: 'Warcraft: O Primeiro Encontro de Dois Mundos',
+    subtitle: 'Warcraft',
+    storyline: 'O mundo dos orcs está à beira da destruição.Liderados pelo mago Guldan, eles entram num portal que os leva a Azeroth, onde se envolvem num conflito com os humanos locais pelo território de Ventobravo',
+    rating: 4.6,
+    imagePath: 'https://cdn.tlc-massive.com/shain/v1/dataservice/ResizeImage/$value?Format=%27png%27&Quality=85&ImageId=%27251486.png%27&ImageUrl=%27251486.png%27&EntityType=%27Item%27&EntityId=%273725%27&Width=1920&Height=1080&device=web_browser&subscriptions=Anonymous',
+  },
+  {
+    title: 'Avengers: Endgame',
+    subtitle: 'Endgame',
+    storyline: 'Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.',
+    rating: 4.6,
+    imagePath: 'https://c7nema.net/wp-content/uploads/2019/04/naom_5cd55a3f25614.jpg',
+  },
+];
+
+export default movies;
